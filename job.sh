f27a4b1b61a35841810c91efc274eabef5c42d45
Ziyong <ziyong@chalmers.se>
2025-04-07 10:59:23 +0200

    delete and don't push job.sh

--- job.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH -A NAISS2025-5-150 -p alvis
-#SBATCH --gpus-per-node=T4:4
-#SBATCH -t 01:00:00
-#SBATCH -J "master_thesis"
-
-srun ./master_thesis.sif python main.py --data-dir "/data" --num-devices 4

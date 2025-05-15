- move `num_decoders` to init of DIB datamodule - simplify datamodule setup - pre-flatten activation and convert to float32 - correct nc_output, should measure *before* last linear (non-softmax) layer - completely revamp NC1 calculation, now uses two-pass algorithm

datasets.py
@@ -52,7 +52,7 @@ class DataModule(LightningDataModule):
         self.batch_size = batch_size
         self.num_devices = num_devices
         self.transform = v2.Compose(
-            [  # TODO: image augmentation and/or normalisation
+            [
                 v2.ToImage(),  # convert to TVTensor Image
                 v2.ToDtype(torch.float, scale=True),  # uint8 {0,…,255} to float32 [0,1]
             ]
@@ -137,14 +137,18 @@ class DataModule(LightningDataModule):
 
 
 class DIBData(DataModule):
-    def __init__(
-        self, dataset: DATASET_TYPE, data_dir: str, batch_size: int, num_devices: int
-    ):
-        super().__init__(dataset, data_dir, batch_size, num_devices)
+    def __init__(self, dm: DataModule):
+        super().__init__(dm.dataset, dm.data_dir, dm.batch_size, dm.num_devices)
+        self.labels = {"train": dm.train_labels, "val": dm.val_labels}
+        self.class_counts = {"train": dm.train_class_counts, "val": dm.val_class_counts}
+        self.num_classes = dm.num_classes
+        self.num_decoders = {
+            # ⌈log_{|Y|}(max{|X_y| : y ∈ Y})⌉ new labels per sample
+            dataset: math.ceil(math.log(class_counts.max(), self.num_classes))
+            for dataset, class_counts in self.class_counts.items()
+        }
 
-    def _base_expand(
-        self, labels: torch.Tensor, class_counts: torch.Tensor, num_classes: int
-    ):
+    def _base_expand(self, labels: torch.Tensor, num_decoders: int):
         r"""
         Generate relabeling `N` of the data where `N[i,:]` are the new labels of the `i`th sample.
         Each sample obtains ⌈log_{|Y|}(max{|X_y| : y ∈ Y})⌉ new labels,
@@ -154,19 +158,18 @@ class DIBData(DataModule):
         the Decodable Information Bottleneck".
         """
         assert labels.dim() == 1, "labels must be 1D"
-        assert torch.max(labels) < num_classes, "labels ⊈ {0,…,num_classes-1}"
-
-        # compute ⌈log_{|Y|}(max{|X_y| : y ∈ Y})⌉
-        num_digits = math.ceil(math.log(class_counts.max(), num_classes))
+        assert torch.max(labels) < self.num_classes, "labels ⊈ {0,…,num_classes-1}"
 
         # enumerate samples of the same class
-        cum_counts = one_hot(labels, num_classes).cumsum(0)
+        cum_counts = one_hot(labels, self.num_classes).cumsum(0)
         idcs = cum_counts[torch.arange(labels.size(0)), labels] - 1
 
-        # base |Y| representation of indices padded with 0s to num_digits digits
-        divisors = torch.tensor([num_classes**i for i in range(num_digits - 1, -1, -1)])
-        temp = idcs.unsqueeze(1).expand(-1, num_digits)
-        return (temp // divisors) % num_classes
+        # base |Y| representation of indices padded with 0s to num_decoders digits
+        divisors = torch.tensor(
+            [self.num_classes**i for i in range(num_decoders - 1, -1, -1)]
+        )
+        temp = idcs.unsqueeze(1).expand(-1, num_decoders)
+        return (temp // divisors) % self.num_classes
 
     def setup(self, stage):
         match stage:
@@ -178,16 +181,11 @@ class DIBData(DataModule):
                 ]
 
                 # create DIB datasets
-                self.train_dib_labels, self.val_dib_labels = (
-                    self._base_expand(labels, class_counts, self.num_classes)
-                    for labels, class_counts in zip(
-                        (self.train_labels, self.val_labels),
-                        (self.train_class_counts, self.val_class_counts),
-                    )
+                train_dib_labels, val_dib_labels = (
+                    self._base_expand(self.labels[dataset], self.num_decoders[dataset])
+                    for dataset in ("train", "val")
                 )
                 self.train = TensorDataset(
-                    self._preprocess(train_ds.data), self.train_dib_labels
+                    self._preprocess(train_ds.data), train_dib_labels
                 )
-                self.val = TensorDataset(
-                    self._preprocess(val_ds.data), self.val_dib_labels
-                )
+                self.val = TensorDataset(self._preprocess(val_ds.data), val_dib_labels)

networks.py
@@ -35,9 +35,10 @@ class _Network(LightningModule):
         num_classes: int,
         total_steps: int,
         no_compile: bool,
+        ignore: list[str] = [],  # ignored hyperparameters
     ):
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters(ignore=ignore)
 
     def configure_model(self):
         self = self.to(memory_format=torch.channels_last)
@@ -113,7 +114,11 @@ class DIBNetwork(_Network):
         Before training, all decoders are reset.
         """
         super().__init__(
-            nn.CrossEntropyLoss, *hyperparams[1:4], total_steps, hyperparams[-1]
+            nn.CrossEntropyLoss,
+            *hyperparams[1:4],
+            total_steps,
+            hyperparams[-1],
+            ["encoder", "decoder"],
         )
         # copy encoder to ensure correct device placement of parameters
         self.encoder = deepcopy(encoder).requires_grad_(False).eval()
@@ -163,8 +168,10 @@ class MetricNetwork(_Network):
 
         # get_hook: layer_name -> (hook: module, args, output -> None)
         def get_hook(name):
-            def hook(_module, _args, output):
-                self.batch_activations[name] = output.detach()
+            def hook(_module, _args, output: torch.Tensor):
+                self.batch_activations[name] = (
+                    output.detach().flatten(start_dim=1).to(torch.float)
+                )
 
             return hook
 
@@ -221,8 +228,8 @@ class MLP(MetricNetwork):
 
         # update return nodes (output hooks)
         self._register_hooks(
-            {f"nc_layer_{i}": block.nl for i, block in enumerate(self.blocks)}
-            | {"nc_output": self.fc}
+            {f"nc_layer_{i}": block for i, block in enumerate(self.blocks[:-1])}
+            | {"nc_output": self.blocks[-1]}
         )
         self.num_blocks = len(self.blocks)
 
@@ -288,20 +295,16 @@ class MNISTNet(MetricNetwork):
 
         # define fully connected layers
         self.classifier = nn.Sequential(
-            OrderedDict(
-                {
-                    "flatten": nn.Flatten(),
-                    "fc1": nn.Linear(conv_final_ndim(), 256),
-                    "nl": nonlinearity(),
-                    "fc2": nn.Linear(256, hyperparams[3]),
-                }
-            )
+            nn.Flatten(),
+            nn.Linear(conv_final_ndim(), 256),
+            nonlinearity(),
+            nn.Linear(256, hyperparams[3]),
         )
 
         # update return nodes (output hooks)
         self._register_hooks(
-            {f"nc_layer_{i}": block.nl for i, block in enumerate(self.blocks)}
-            | {"nc_output": self.classifier.fc2}
+            {f"nc_layer_{i}": block for i, block in enumerate(self.blocks)}
+            | {"nc_output": self.classifier[2]}
         )
         self.num_blocks = len(self.blocks)
 
@@ -344,10 +347,12 @@ class Block(nn.Module):
 class MaxPoolFC(nn.Module):
     def __init__(self, in_features: int, out_features: int) -> None:
         super().__init__()
+        self.dummy = nn.Identity()  # provides output for the hook
         self.fc = nn.Linear(in_features, out_features)
 
     def forward(self, x: torch.Tensor):
         x = x.amax((-2, -1))  # global max pooling (N, C, H, W) -> (N, C)
+        x = self.dummy(x)
         return self.fc(x)
 
 
@@ -374,7 +379,7 @@ class CIFARNet(MetricNetwork):
         # update return nodes (output hooks)
         self._register_hooks(
             {f"nc_layer_{i}": block for i, block in enumerate(self.blocks)}
-            | {"nc_output": self.head.fc}
+            | {"nc_output": self.head.dummy}
         )
         self.num_blocks = len(self.blocks)
 
@@ -395,20 +400,17 @@ class ConvNeXt(MetricNetwork):
         # import torchvision model
         self.convnext = convnext_tiny(num_classes=hyperparams[3])
 
-        # replace first and last layer to match input and output shapes
-        old_conv1 = self.convnext.features[0][0]
+        # replace first layer to match input shape
+        # (parameters copied from the original model)
         self.convnext.features[0][0] = nn.Conv2d(
-            in_channels=in_shape[0],
-            out_channels=old_conv1.out_channels,
-            kernel_size=old_conv1.kernel_size,
-            stride=old_conv1.stride,
+            in_channels=in_shape[0], out_channels=96, kernel_size=4, stride=4
         )
 
         # update return nodes (output hooks)
         new_hooks = {}
         for i, j in zip(range(4), (2, 2, 8, 2)):
             new_hooks[f"nc_layer_{i}"] = self.convnext.features[2 * i + 1][j]
-        new_hooks["nc_output"] = self.convnext.classifier[2]
+        new_hooks["nc_output"] = self.convnext.classifier[1]
         self._register_hooks(new_hooks)
         self.num_blocks = len(self.convnext.features) // 2
 
@@ -433,15 +435,15 @@ class ResNet(MetricNetwork):
         # import torchvision model
         self.resnet = resnet18(num_classes=hyperparams[3])
 
-        # replace first and last layer to match input and output shapes
-        old_conv1 = self.resnet.conv1
+        # replace first layer to match input shape
+        # (parameters copied from the original model)
         self.resnet.conv1 = nn.Conv2d(
             in_channels=in_shape[0],
-            out_channels=old_conv1.out_channels,
-            kernel_size=old_conv1.kernel_size,
-            stride=old_conv1.stride,
-            padding=old_conv1.padding,
-            bias=old_conv1.bias is not None,
+            out_channels=64,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False,
         )
 
         # update return nodes (output hooks)
@@ -449,7 +451,7 @@ class ResNet(MetricNetwork):
         for i in range(4):
             layer = getattr(self.resnet, f"layer{i + 1}")
             new_hooks[f"nc_layer_{i}"] = layer[1].relu
-        new_hooks["nc_output"] = self.resnet.fc
+        new_hooks["nc_output"] = self.resnet.avgpool
         self._register_hooks(new_hooks)
         self.num_blocks = 4
 

utils.py
@@ -132,34 +132,28 @@ class ComputeNC1(Callback):
         num_classes: int,
     ):
         """
-        Store metrics for each layer as dict `layer_metrics`.
-        Let zˡ be the size of the flattened activations of layer l and
-        C the number of classes, then
-
-        - `size(layer_metrics[l]) == (C + zˡ, zˡ)`
-        - `layer_metrics[l][:C]` are the per-class activation sums
-        - `layer_metrics[l][C:]` is the gram matrix
+        Store per-class activation sums for each layer as dict `all_class_sums`.
+        `all_class_sums[l]` is of size (C, zˡ) where C is the number of classes and
+        zˡ is the size of the flattened activations of layer l.
         """
         self.train_class_counts = train_class_counts
         self.val_class_counts = val_class_counts
         self.num_classes = num_classes
-        self.layer_metrics: dict[str, torch.Tensor] = {}
+        self.all_class_sums: dict[str, torch.Tensor] = {}
 
     @torch.inference_mode()
     def _on_batch_end(self, network: MetricNetwork, batch: list[torch.Tensor]):
         """Update NC metrics after each batch"""
         _, targets = batch
         for layer, activations in network.batch_activations.items():
-            activations = activations.flatten(start_dim=1)
-            if layer not in self.layer_metrics:
-                act_size = activations.size(1)
-                self.layer_metrics[layer] = torch.zeros(
-                    (self.num_classes + act_size, act_size), device=network.device
+            if layer not in self.all_class_sums:
+                self.all_class_sums[layer] = activations.new_zeros(
+                    (self.num_classes, activations.size(1))
                 )
-            self.layer_metrics[layer][: self.num_classes].index_add_(
+            self.all_class_sums[layer].index_add_(
                 dim=0, index=targets, source=activations
             )
-            self.layer_metrics[layer][self.num_classes :] += activations.T @ activations
+        network.batch_activations.clear()
 
     def on_train_batch_end(
         self,
@@ -183,47 +177,86 @@ class ComputeNC1(Callback):
 
     @torch.inference_mode()
     def _on_epoch_end(
-        self, network: MetricNetwork, state: str, class_counts: torch.Tensor
+        self,
+        network: MetricNetwork,
+        state: str,
+        class_counts: torch.Tensor,
+        dataloader,
     ):
         """Aggregate batched NC metrics and log them"""
 
-        # TODO: come up with smarter way to load-balance
-        # synchronise metrics to process 0
+        # synchronise metrics
         if torch.distributed.is_available() and torch.distributed.is_initialized():
-            for joint_metric in self.layer_metrics.values():
-                torch.distributed.reduce(joint_metric, dst=0)
+            for class_sums in self.all_class_sums.values():
+                torch.distributed.all_reduce(class_sums)
+        class_counts = class_counts.to(network.device)
 
-        # only compute and log nc on process 0
+        class_means: dict[str, torch.Tensor] = {}
+        centred_means: dict[str, torch.Tensor] = {}
+        rev_between_covs = []
+
+        # compute means and reversed covariances
+        total_count = class_counts.sum()
+        for i, (layer, class_sums) in enumerate(self.all_class_sums.items()):
+            class_means[layer] = class_sums / class_counts.unsqueeze(
+                dim=1
+            )  # [𝛍_1^l ⋯ 𝛍_C^l]^⊤: (C, zˡ)
+            global_mean = class_sums.sum(dim=0) / total_count  # ̄𝛍ˡ: (zˡ)
+            centred_means[layer] = class_means[layer] - global_mean  # (𝐌ˡ)^⊤: (C, zˡ)
+            rev_between_covs.append(
+                centred_means[layer] @ centred_means[layer].T
+            )  # (𝐌ˡ)^⊤ 𝐌ˡ: (C, C), recall: Σ_B^l = 1/C 𝐌ˡ(𝐌ˡ)^⊤
+
+        # compute eigendecomposition of (𝐌ˡ)^⊤(𝐌ˡ)
+        eigvals, rev_eigvecs = torch.linalg.eigh(
+            torch.stack(rev_between_covs, dim=0)
+        )  # (n_layers, C), (n_layers, C, C)
+
+        # second pass
+        nc = {f"{layer}_{state}": 0 for layer in self.all_class_sums.keys()}
+        for batch in dataloader:
+            inputs, targets = network.transfer_batch_to_device(batch, network.device, 0)
+
+            # compute activations
+            with torch.autocast(device_type=network.device.type, dtype=network.dtype):
+                network(inputs)
+
+            # aggregate second pass
+            for i, (layer, activations) in enumerate(network.batch_activations.items()):
+                matrix_prod = (
+                    (activations - class_means[layer][targets]) @ centred_means[layer].T
+                ) @ rev_eigvecs[i]  # (𝐡_c^l - 𝛍_c^l)^⊤ 𝐌ˡ 𝐕: (batch, C)
+
+                # filter out eigenvalues close to zero for stability
+                mask = eigvals[i] > 1e-5 * eigvals[i].max()
+                nc[f"{layer}_{state}"] += (
+                    (matrix_prod[:, mask] / eigvals[i, mask]) ** 2
+                ).sum()
+
+        # synchronise metrics again
+        if torch.distributed.is_available() and torch.distributed.is_initialized():
+            for nc_value in nc.values():
+                torch.distributed.all_reduce(nc_value)
+
+        # final factor
+        nc = {key: value * self.num_classes / total_count for key, value in nc.items()}
+
+        # only log on process 0 since value is the same for all processes
         if network.global_rank == 0:
-            nc: dict[str, torch.Tensor] = {}
-            class_counts = class_counts.to(network.device)
-            total_count = class_counts.sum()
-            for layer, joint_metric in self.layer_metrics.items():
-                class_sums = joint_metric[: self.num_classes]
-                class_means = class_sums / class_counts.unsqueeze(dim=1)
-                global_mean = class_sums.sum(dim=0) / total_count
-                centred_means = class_means - global_mean
-                between_cov = (centred_means.T @ centred_means) / self.num_classes
-                within_cov = (
-                    joint_metric[self.num_classes :] / total_count
-                    - torch.outer(global_mean, global_mean)
-                    - between_cov
-                )
-                nc[f"{layer}_{state}"] = torch.linalg.lstsq(
-                    between_cov.cpu(),  # gelsd only supported on CPU
-                    within_cov.cpu(),  # gelsd only supported on CPU
-                    driver="gelsd",
-                ).solution.trace()
             network.log_dict(nc, rank_zero_only=True)
 
         # reset NC metrics for next epoch
-        self.layer_metrics.clear()
+        self.all_class_sums.clear()
 
-    def on_train_epoch_end(self, _trainer, network: MetricNetwork):
-        self._on_epoch_end(network, "train", self.train_class_counts)
+    def on_train_epoch_end(self, trainer: Trainer, network: MetricNetwork):
+        self._on_epoch_end(
+            network, "train", self.train_class_counts, trainer.train_dataloader
+        )
 
-    def on_validation_epoch_end(self, _trainer, network: MetricNetwork):
-        self._on_epoch_end(network, "val", self.val_class_counts)
+    def on_validation_epoch_end(self, trainer: Trainer, network: MetricNetwork):
+        self._on_epoch_end(
+            network, "val", self.val_class_counts, trainer.val_dataloaders
+        )
 
     def on_validation_start(self, _trainer, network: MetricNetwork):
         """
@@ -231,15 +264,12 @@ class ComputeNC1(Callback):
         Necessary since validation is done before the end of each training epoch
         (but after all training batches).
         """
-        self.train_batch_activations = deepcopy(network.batch_activations)
-        self.train_layer_metrics = deepcopy(self.layer_metrics)
-        network.batch_activations.clear()
-        self.layer_metrics.clear()
+        self.train_class_sums = deepcopy(self.all_class_sums)
+        self.all_class_sums.clear()
 
     def on_validation_end(self, _trainer, network: MetricNetwork):
         """Restore training metrics"""
-        network.batch_activations = self.train_batch_activations
-        self.layer_metrics = self.train_layer_metrics
+        self.all_class_sums = self.train_class_sums
 
 
 class ComputeDIB(Callback):
@@ -252,8 +282,12 @@ class ComputeDIB(Callback):
         num_devices: int,
         *block_indices: int,
     ):
-        self.dib_epochs = dib_epochs
+        # compute DIB data metrics
         self.dib_dm = dib_dm
+        self.dib_dm.prepare_data()
+        self.dib_dm.setup("fit")
+
+        self.dib_epochs = dib_epochs
         self.num_devices = num_devices
         self.block_indices = block_indices
         self.dib_nets: dict[str, list[DIBNetwork]] = {}
@@ -262,12 +296,8 @@ class ComputeDIB(Callback):
         """Create DIB networks for each block"""
 
         # default to all blocks if no block indices were given
-        if not self.block_indices :
+        if not self.block_indices:
             self.block_indices = list(range(network.num_blocks + 1))
-
-        # compute DIB data metrics
-        self.dib_dm.prepare_data()
-        self.dib_dm.setup("fit")
 
         # create DIB networks for each block for both datasets
         hyperparams = tuple(network.hparams_initial.values())
@@ -277,7 +307,7 @@ class ComputeDIB(Callback):
             self.dib_nets[dataset] = [
                 DIBNetwork(
                     *network.get_encoder_decoder(block_idx),
-                    getattr(self.dib_dm, f"{dataset}_dib_labels").size(1),
+                    self.dib_dm.num_decoders[dataset],
                     total_steps,
                     hyperparams,
                 )
@@ -303,7 +333,7 @@ class ComputeDIB(Callback):
                 # train DIB network
                 dib_trainer = Trainer(
                     devices=self.num_devices,
-                    precision="bf16-mixed",
+                    precision="bf16-true",
                     max_epochs=self.dib_epochs,
                     logger=False,  # don't write (but do store) training losses
                     default_root_dir="lightning_logs",